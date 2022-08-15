607d99891ad7cb27df4c6f09675ba9635b8795d7
caitevanish <96436738+caitevanish@users.noreply.github.com>
2022-08-15 13:48:07 -0700

    Set up basic project folders (app, entities). Added dependencies.

---
+++ src/main/java/dev/evanishyn/app/App.java
@@ -0,0 +1,8 @@
+package dev.evanishyn.app;
+
+public class App {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/dev/evanishyn/entities/Constituent.java
@@ -0,0 +1,4 @@
+package dev.evanishyn.entities;
+
+public class Constituent {
+}
